e2308fafe3ce8c06d540a9cea67d393f5cce9780
Jakubem <jakubmx@gmail.com>
2018-02-22 00:55:23 +0100

    fix malfunctioning routing

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.get('/photo', function(req, res){
   );
 });
 
-app.get('/about', function(req, res){
-  res.send(
-    pug.renderFile('public/about.pug', {
-    })
-  );
-});
+// app.get('/about', function(req, res){
+//   res.send(
+//     pug.renderFile('public/about.pug', {
+//     })
+//   );
+// });
 
 app.get('/video', function(req, res){
   res.send(
@@ -34,17 +34,15 @@ app.get('/video', function(req, res){
   );
 });
 
-app.get('/stuff', function(req, res){
+app.get('/projects', function(req, res){
   res.send(
-    pug.renderFile('public/stuff.pug', {
+    pug.renderFile('public/projects.pug', {
     })
   );
 });
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
   console.log('listening on port 3000')
 })
-
