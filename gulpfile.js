854e39763e86d52bd9b75380a30770d724a65782
Jakubem <jakubmx@gmail.com>
2018-02-23 23:40:49 +0100

    major update of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,8 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
-var watch = require('gulp-watch');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
 
-var buildPage = function () {
-  return gulp.src(['public/*.pug'])
-    .pipe(pug({pretty:true}))
-    .pipe(gulp.dest('pug-to-html'))
-}
-gulp.task('build', buildPage)
-
-gulp.task('watch', function () {
-  return watch('public/**/*.pug', buildPage)
-});
-
-gulp.task('sass', function () {
-  return gulp.src('./public/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+gulp.task('pug', function () {
+  return gulp.src('./public/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./dest/'))
 });
