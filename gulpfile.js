282fab49ab7473d232ce3b69feb39222415ec0c7
Jakubem <jakubmx@gmail.com>
2018-03-05 23:08:48 +0100

    remove gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-
-gulp.task('pug', function () {
-  return gulp.src('./public/*.pug')
-  .pipe(pug())
-  .pipe(gulp.dest('./dest/'))
-});
