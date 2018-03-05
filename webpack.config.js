11ea78382eb85fc1f0755645f4cbdde1f5617031
Jakubem <jakubmx@gmail.com>
2018-03-05 21:40:31 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+ 
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
