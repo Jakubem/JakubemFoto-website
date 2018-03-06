07242af572792b09e9e28071c7a44da3b80f3ab0
Jakubem <jakubmx@gmail.com>
2018-03-06 21:04:06 +0100

    webpack setup kinda working ¯\_(ツ)_/¯

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-const path = require('path');
- 
+const path = require('path'); 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/js/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ]
     }
-}
+};
