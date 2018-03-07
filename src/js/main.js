c2e68155a2c04b4dbee5c3c8838b1d89d20fbdf8
Jakubem <jakubmx@gmail.com>
2018-03-07 01:01:26 +0100

    for now comment out function requiring all pugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 const js = require('./index.js');
 const css = require('../scss/style.scss');
+
+// function requireAll(r) {
+//     r.keys().forEach(r);
+// }
+// requireAll(require.context('./', true, /\.pug$/));
