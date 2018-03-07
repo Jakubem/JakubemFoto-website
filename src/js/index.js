782d530f4ba88005df0dada81de4bf4c47a79fd4
Jakubem <jakubmx@gmail.com>
2018-03-07 20:02:18 +0100

    fix path to halkaBox scripts

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
-const halkaBox = require('./halkaBox.min.js');
-  halkaBox.run("gallery1", {
-    hideButtons: false,
-    animation: "slide",
-    theme: "dark",
-    preload: 2
-  });
+const halkaBox = require('./halkaBox').halkaBox;
 
-  halkaBox.run("gallery2", {
-    hideButtons: false,
-    animation: "slide",
-    theme: "dark",
-    preload: 2
-  });
+halkaBox.run("gallery1", {
+  hideButtons: false,
+  animation: "slide",
+  theme: "dark",
+  preload: 2
+});
 
-  const btn = document.querySelector('.btn');
-  const hdr = document.querySelector('.menu-list');
-  btn.addEventListener('click', function () {
-    hdr.classList.toggle('open');
-  });
+halkaBox.run("gallery2", {
+  hideButtons: false,
+  animation: "slide",
+  theme: "dark",
+  preload: 2
+});
+
+const btn = document.querySelector('.btn');
+const hdr = document.querySelector('.menu-list');
+btn.addEventListener('click', function () {
+  hdr.classList.toggle('open');
+});
