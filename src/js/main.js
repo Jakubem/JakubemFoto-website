fc70a422f341801ce0f6d699af74b8f37b74aa73
Jakubem <jakubmx@gmail.com>
2018-03-07 20:02:40 +0100

    remove comments for pug

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,2 @@
 const js = require('./index.js');
 const css = require('../scss/style.scss');
-
-// function requireAll(r) {
-//     r.keys().forEach(r);
-// }
-// requireAll(require.context('./', true, /\.pug$/));
