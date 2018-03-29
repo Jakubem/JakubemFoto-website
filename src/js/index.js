45d08334d7b9777610e88ac8bf8f6c338486010f
Jakubem <jakubmx@gmail.com>
2018-03-29 22:48:12 +0200

    add 'ludzie' gallery

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,12 @@ halkaBox.run("gallery2", {
   theme: "dark",
   preload: 1
 });
+halkaBox.run("gallery3", {
+  hideButtons: false,
+  animation: "slide",
+  theme: "light",
+  preload: 1
+});
 
 const btn = document.querySelector('.btn');
 const hdr = document.querySelector('.menu-list');
