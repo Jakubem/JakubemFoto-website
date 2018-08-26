1fc1576c95170eea522820767d7feb1782951aec
Jakubem <jakubmx@gmail.com>
2018-08-27 01:16:46 +0200

    fix paths in server bundle

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const pug = require('pug')
 
@@ -35,8 +36,8 @@ app.get('/projects', (req, res) => {
   );
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
